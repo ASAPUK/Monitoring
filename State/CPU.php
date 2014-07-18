0ab52d5fc9ee4ff099654e009dc7317a70793eb1
Sergey Miroshnikov <hyperxai@gmail.com>
2014-07-18 14:33:56 +0300

    cpu

--- State/CPU.php
+++ State/CPU.php
@@ -12,6 +12,7 @@ class CPU extends StateAbstract
 {
     const STATE_TYPE         = 'CPU';
     const CPU_PROCESS_NUMBER = 'max_cpu_process_number';
+    const TIME_TYPE          = 'time';
     protected $_default = array(
         self::CPU_PROCESS_NUMBER => 80
     );
@@ -21,9 +22,9 @@ class CPU extends StateAbstract
         $load = sys_getloadavg();
         $CPUProcessNumber = $this->getMaxCPUProcessNumbers();
 
-        if ($load[0] > $CPUProcessNumber ) {
+        if ($load[$this->getTime()] > $CPUProcessNumber ) {
             $this->getHandler()->addErrorHandle(
-                "Current CPU process number is {$load[0]}, when allowed is {$CPUProcessNumber}",
+                "Current CPU process number is {$load[$this->getTime()]}, when allowed is {$CPUProcessNumber}",
                 time(),
                 $this->getStateType()
             );
@@ -33,5 +34,22 @@ class CPU extends StateAbstract
     private function getMaxCPUProcessNumbers()
     {
         return $this->getParam( self::CPU_PROCESS_NUMBER );
+    }
+
+    private function getTime()
+    {
+        switch (self::TIME) {
+            case 1:
+                return 0;
+                break;
+            case 5:
+                return 1;
+                break;
+            case 15:
+                return 2;
+                break;
+            default:
+                return 0;
+        }
     }
 }
