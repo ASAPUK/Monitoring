9acd26fed0cb49c610c1f72a1dd76409d3869ebf
Sergey Miroshnikov <hyperxai@gmail.com>
2014-08-21 17:51:22 +0300

    error logger

--- ErrorLogger.php
+++ ErrorLogger.php
@@ -2,7 +2,134 @@
 
 namespace Monitoring;
 
-class ErrorLogger
+class ErrorLogger extends \Monitoring\Singleton
 {
+    const ENABLED = 'enabled';
+    const PATH    = 'path';
+    const DATE_FORMAT = 'date_format';
 
+    const ERROR   = 'error';
+    const WARNING = 'warning';
+
+    const FORMAT = 'Y-m-d H:i:s';
+
+    private $path;
+    private $enabled = false;
+    private $dateFormat = self::FORMAT;
+
+    public function setConfig(array $config)
+    {
+        if ( isset($config[self::ENABLED]) && isset($config[self::PATH]) ) {
+            $this->enabled = $config[self::ENABLED];
+            $this->path = $config[self::PATH];
+
+            if ( isset($config[self::DATE_FORMAT]) ) {
+                $this->dateFormat = $config[self::DATE_FORMAT];
+            }
+        }
+    }
+
+    public function handleError($msg, $type = self::ERROR)
+    {
+        if ( !$this->getEnabled() ) return;
+
+        $this->createPathIfNotExist();
+
+        $error = array(
+            'time'    => date($this->dateFormat),
+            'message' => $msg,
+            'type'    => $type
+        );
+
+        $path = $this->getPath();
+
+        $fp = fopen($path, 'a');
+        fputcsv($fp, $error);
+        fclose($fp);
+    }
+
+    private function createPathIfNotExist()
+    {
+        $path = $this->getPath();
+        if( !file_exists(dirname($path)) ) {
+            $arr  = explode('/', dirname($path));
+            $curr = array();
+
+            foreach($arr as $val){
+                $curr[] = $val;
+                $path = implode('/', $curr) . '/';
+                if ( !file_exists($path) ) {
+                    mkdir($path, 0766);
+                    @chmod($path, 0766);
+                }
+            }
+        }
+    }
+
+    public function clearFile()
+    {
+        if (!$this->getEnabled()) return;
+
+        $fp = fopen($this->getPath(), 'w');
+        fputcsv($fp, '');
+        fclose($fp);
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    public function getErrors()
+    {
+        return $this->getErrorsByType(array(self::ERROR));
+
+    }
+
+    public function getWarnings()
+    {
+        return $this->getErrorsByType(array(self::WARNING));
+
+    }
+
+    public function getAllErrors()
+    {
+        return $this->getErrorsByType();
+    }
+
+    private function getErrorsByType(array $types = array(self::ERROR, self::WARNING))
+    {
+        $result = array();
+        if (!$this->getEnabled()) return $result;
+
+        $delimiter = ",";
+        $file = new \SplFileObject($this->getPath(), 'r');
+        $file->setFlags(\SplFileObject::READ_CSV);
+        $file->setCsvControl($delimiter);
+
+        while (!$file->eof()) {
+            $current = $file->current();
+            if ( in_array($current[2], $types) ) {
+                $result[] = $current;
+            }
+            $file->next();
+        }
+
+        return $result;
+    }
+
+    public function parseMessage(array $error)
+    {
+        return isset($error[1]) ? $error[1] : null;
+    }
+
+    public function parseDate(array $error)
+    {
+        return isset($error[0]) ? $error[0] : null;
+    }
 }
--- State/ErrorLogger.php
+++ State/ErrorLogger.php
@@ -2,87 +2,30 @@
 
 namespace Monitoring\State;
 
+use Monitoring\ErrorLogger as MErrorLogger;
+
 /**
  * Class ErrorLogger
  * @package Monitoring\State
  */
 class ErrorLogger extends StateAbstract
 {
-    const STATE_TYPE = 'YiiApplication';
-    const PATH       = 'path';
-    const STATUS     = 'status';
-    const COUNT      = 'count';
-    const TIME       = 'time';
-
-    protected $_default = array(
-        self::STATUS => array('error'),
-        self::COUNT  => 200,
-        self::TIME   => 3600
-    );
+    const STATE_TYPE = 'Error';
+    const PATH = 'path';
 
     public function verifyError()
     {
-        $path = $this->getParam(self::PATH, null);
-        $allowedStatuses = $this->getParam(self::STATUS);
+        $logger = MErrorLogger::getInstance();
+        $logger->setConfig($this->getParams());
 
-        if (!file_exists($path)) return;
-        exec("tail -n {$this->getParam(self::COUNT)} {$path}", $tail);
+        $errors = $logger->getErrors();
 
-        if ( count($tail) == 0 ) return;
-
-        $log = $buffer = array();
-        foreach (array_reverse($tail) as $str) {
-            $buffer[] = $str;
-            if ( preg_match('#\d{4}/\d{2}/\d{2}#', $str) ) {
-                $log[] = implode("\r\n", array_reverse($buffer));
-                $buffer = array();
+        if ( count($errors) > 0 ) {
+            foreach ($errors as $error) {
+                $this->getHandler()->addErrorHandle( $logger->parseMessage($error), $logger->parseDate($error), $this->getStateType() );
             }
         }
 
-        $allowedTime = $this->getParam(self::TIME);
-        foreach ($log as $l) {
-            $status = $this->showStatus($l);
-            $date   = strtotime($this->showDate($l));
-
-            if ( in_array($status, $allowedStatuses) && (time() - $date < $allowedTime) ) {
-                $error  = $this->showError($l) . $this->showStack($l);
-
-                $this->getHandler()->addErrorHandle( $error, $date, $this->getStateType() );
-            }
-        }
-    }
-
-    private function showStack($text)
-    {
-        $text = explode('Stack trace:', $text);
-        return @$text[1];
-    }
-
-    private function showDate($text)
-    {
-        return date('H:i d.m.Y', strtotime(mb_substr($text, 0, 20,'utf8')));
-    }
-
-    private function showError($text)
-    {
-        $text = mb_substr($text, 20, mb_strlen($text,'utf8'),'utf8');
-
-        $text = explode('Stack trace:', $text);
-        $text = $text[0];
-
-        return $text;
-    }
-
-    private function showStatus($text)
-    {
-        if (preg_match('%\[error\]%',$text)) {
-            return 'error';
-        } elseif (preg_match('%\[warning\]%',$text)) {
-            return 'warning';
-        } elseif (preg_match('%\[info\]%',$text)) {
-            return 'info';
-        }else {
-            return 'undefined';
-        }
+        $logger->clearFile();
     }
 }
